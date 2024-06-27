d3a0ca63c832e8e5d522bc7426a7da1ad924e17d
JamesZam96 <jameszamgm@gmail.com>
2024-06-27 12:17:21 -0500

    pruebaapi

---
+++ database/migrations/2024_06_27_151726_create_posts_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use App\Models\Post;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('slug');
+            $table->string('extract');
+            $table->string('body');
+            $table->enum('status',[Post::BORRADO,Post::PUBLICADO])->default(Post::BORRADO)->nullable();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->foreignId('category_id')->constrained()->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('posts');
+    }
+};
